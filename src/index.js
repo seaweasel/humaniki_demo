fbb00dd49bd3426345b7308281742145b4d6e2ad
seaweawsel <na@na.na>
2020-08-04 11:01:01 -0400

    ci push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Away We Go on ${process.env.PORT} `);
+    console.log(`Away We Go on ${process.env.PORT}`);
 });
